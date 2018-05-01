e1374b62486ad4e3186121614677f8dc61400c6e
Simon Roberts <simon@dancingcloudservices.com>
2018-05-01 09:40:00 -0600

    End of DateTime discussion

--- src/main/java/datetime/Ex1.java
+++ src/main/java/datetime/Ex1.java
@@ -1,6 +1,7 @@
 package datetime;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 
@@ -28,5 +29,22 @@ public class Ex1 {
 //    System.out.println(form); formatter converts
     System.out.println(form.format(today));
     System.out.println(form.format(yesterday));
+
+    Period p = Period.of(2, 20, 100);
+    System.out.println(p);
+    Period pNorm = p.normalized();
+    System.out.println(pNorm);
+    // A) P2Y20M100D
+    // B) P3Y11M10D
+    // C) P3Y8M100D
+    // D) Something else, including errors
+
+    Period sixty = Period.ofDays(60);
+    LocalDate febOneNonLeap = LocalDate.of(2018, 2, 1);
+    LocalDate marOne = LocalDate.of(2018, 3, 1);
+    System.out.println("feb one: " + febOneNonLeap
+        + " sixty later is: " + febOneNonLeap.plus(sixty));
+    System.out.println("mar one: " + marOne
+        + " sixty later is: " + marOne.plus(sixty));
   }
 }
