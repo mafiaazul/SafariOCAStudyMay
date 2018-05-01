ee901e016499ec4f6def7714b6e29b9ccb9aee47
Rocha, Rodrigo <Rocha.Rodrigo@principal.com>
2018-05-01 15:08:13 -0500

    2nd commit

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -2,6 +2,10 @@ package hello;
 
 public class Hello {
   public static void main(String[] args) {
-    ((Runnable)()-> System.out.println("Hello world!")).run();
+
+    String s1 = "Hello";
+    String s2 = "Hello";
+      System.out.println("Test");
+      System.out.println("s1 == s2" + s1 == s2 );
   }
 }
